c67ddd04546d9cbe7f6cc44a4eed151700dd9376
Michal Kononenko <michalkononenko@gmail.com>
2017-06-07 10:36:16 -0400

    Wrote javadoc for the JSON parser

--- src/main/java/ca/uwaterloo/iqc/topchef/adapters/com/fasterxml/jackson/core/ObjectMapper.java
+++ src/main/java/ca/uwaterloo/iqc/topchef/adapters/com/fasterxml/jackson/core/ObjectMapper.java
@@ -13,16 +13,57 @@ import java.io.Writer;
  * Describes the interface to Jackson's JSON writer
  */
 public interface ObjectMapper {
-    Object readValue(String JSON) throws IOException, JsonParseException, JsonMappingException;
+    /**
+     *
+     * @param JSON The JSON object to deserialize as a string
+     * @return The JSON object as a {@link Object}
+     * @throws IOException If the data cannot be read
+     * @throws JsonParseException If the incoming JSON cannot be parsed
+     */
+    Object readValue(String JSON) throws IOException, JsonParseException;
 
+    /**
+     *
+     * @param JSON The JSON object to deserialize as a string
+     * @param typeOfJSON The class to which this object is to be deserialized
+     * @param <T> The type of object that this method should return
+     * @return An object of the required type, built from the given JSON
+     * @throws IOException If the data cannot be read
+     * @throws JsonParseException If syntactically-incorrect JSON was sent to this device
+     * @throws JsonMappingException I
+     */
     <T> T readValue(String JSON, Class<T> typeOfJSON) throws IOException, JsonParseException, JsonMappingException;
 
+    /**
+     *
+     * @param stream A character stream from which JSON is to be read.
+     * @param typeOfJSON The class to which the result is to be marshalled
+     * @param <T> The type to which data is to be marshalled
+     * @return The desired instance, built using data from this JSON
+     * @throws IOException If the data could not be read
+     * @throws JsonParseException If syntactically-incorrect JSON is sent in
+     * @throws JsonMappingException If good JSON is sent in, but it doesn't map to a property of ``T``
+     */
     <T> T readValue(InputStream stream, Class<T> typeOfJSON) throws IOException, JsonParseException,
             JsonMappingException;
 
     String writeValueAsString(Object value) throws JsonProcessingException;
 
+    /**
+     *
+     * @param writer The {@link Writer} to which this data is to be written
+     * @param value The object that needs to be expressed as JSON
+     * @throws IOException If the underlying {@link Writer} throws an {@link IOException}
+     * @throws JsonProcessingException If there is a problem processing the JSON
+     */
     void writeValue(Writer writer, Object value) throws IOException;
 
+    /**
+     *
+     * @param stream The {@link OutputStream} to which this data is to be written
+     * @param value The object to be expressed as JSON
+     * @throws IOException If the underlying {@link Writer} throws an {@link IOException}
+     * @throws JsonProcessingException If there is a problem processing the JSON
+     */
     void writeValue(OutputStream stream, Object value) throws IOException;
 }
