a1bc65fb596334a22e7352a170602116c0541f2c
Michal Kononenko <michalkononenko@gmail.com>
2017-06-09 10:55:42 -0400

    Added some javadoc to the service endpoint

--- src/main/java/ca/uwaterloo/iqc/topchef/endpoints/ServiceEndpoint.java
+++ src/main/java/ca/uwaterloo/iqc/topchef/endpoints/ServiceEndpoint.java
@@ -19,24 +19,53 @@ import java.util.UUID;
  * Describes an endpoint with service data, to which jobs can be submitted
  */
 public class ServiceEndpoint extends AbstractMutableJSONEndpoint implements Service {
+    /**
+     * The UUID of this service, used to uniquely refer to this service
+     *
+     * -- GETTER --
+     *
+     *  @return the service ID
+     */
     @Getter
     private final UUID serviceID;
 
+    /**
+     *
+     * @param client The client under which this service was created
+     * @param id The service ID
+     */
     public ServiceEndpoint(Client client, UUID id){
         super(client.getURLResolver().getServiceEndpoint(id));
         this.serviceID = id;
     }
 
+    /**
+     *
+     * @param client The client under which this service was created
+     * @param id The service ID
+     * @throws IllegalArgumentException If the string provided is not a valid UUID
+     */
     public ServiceEndpoint(Client client, String id) throws IllegalArgumentException {
         super(client.getURLResolver().getServiceEndpoint(UUID.fromString(id)));
         this.serviceID = UUID.fromString(id);
     }
 
+    /**
+     *
+     * @return The schema that must be matched in order to allow posting a new job
+     * @throws HTTPException If the server does something weird
+     * @throws IOException If I/O from this machine to the server does something weird
+     */
     @Override
     public Object getJobRegistrationSchema() throws HTTPException, IOException {
         return getServiceData().getJob_registration_schema();
     }
 
+    /**
+     * Let the server know that this service has not timed out yet
+     * @throws HTTPException If the server screws up
+     * @throws IOException If I screw up
+     */
     @Override
     public void checkIn() throws HTTPException, IOException {
         @Cleanup URLConnection connection = getPatchRequestForCheckIn(this.getURL());
@@ -44,10 +73,23 @@ public class ServiceEndpoint extends AbstractMutableJSONEndpoint implements Serv
         assertGoodResponseCode(connection);
     }
 
+    /**
+     *
+     * @return The data from running a GET request to /services/(service_id)
+     * @throws HTTPException If the server does something weird
+     * @throws IOException If I/O from this machine to the server does something weird
+     */
     private ServiceData getServiceData() throws IOException, HTTPException {
         return this.getJSON(ServiceData.class);
     }
 
+    /**
+     * Return a connection configured to make a PATCH request with no request body to the
+     * service endpoint. This is needed in order to check a service in with the server
+     * @param url The URL where the checkin is to take place
+     * @return An open connection to the desired {@link URL} which is properly configured
+     * @throws IOException If the connection cannot be opened
+     */
     private static URLConnection getPatchRequestForCheckIn(URL url) throws IOException {
         URLConnection connection;
 
@@ -63,6 +105,9 @@ public class ServiceEndpoint extends AbstractMutableJSONEndpoint implements Serv
         return connection;
     }
 
+    /**
+     * The template for the response JSON from a GET request to /services/(service_id)
+     */
     public static class ServiceData {
         @Getter
         @Setter
