2ae37dc02da9c60ca650d73c6295170dc79d7f78
Michal Kononenko <michalkononenko@gmail.com>
2017-06-12 17:01:22 -0400

    Removed unused import in the Abstract services endpoint test case integration test.

--- src/test/java/ca/uwaterloo/iqc/topchef/test/integration/endpoints/services_endpoint/AbstractServicesEndpointTestCase.java
+++ src/test/java/ca/uwaterloo/iqc/topchef/test/integration/endpoints/services_endpoint/AbstractServicesEndpointTestCase.java
@@ -1,7 +1,6 @@
 package ca.uwaterloo.iqc.topchef.test.integration.endpoints.services_endpoint;
 
 import ca.uwaterloo.iqc.topchef.test.integration.endpoints.AbstractEndpointsTestCase;
-import org.junit.Test;
 
 /**
  * Base class for integration tests of {@link ca.uwaterloo.iqc.topchef.endpoints.ServicesEndpoint}
